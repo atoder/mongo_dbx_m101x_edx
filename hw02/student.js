1f35f92174d06a054f33f0d2c3074e48d12ae056
atoder <alexandert@gmail.com>
2016-06-03 20:03:34 -0700

    Adding firstName and lastName virtuals

---
+++ hw02/student.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+  name: { type: String, required: true },
+  courses: [{ type: String, ref: 'Course' }]
+});
+
+/* Returns the student's first name, which we will define
+ * to be everything up to the first space in the student's name.
+ * For instance, "William Bruce Bailey" -> "William" */
+schema.virtual('firstName').get(function() {
+  return this.name.split(' ').slice(0,1).join(' ');
+});
+
+/* Returns the student's last name, which we will define
+ * to be everything after the last space in the student's name.
+ * For instance, "William Bruce Bailey" -> "Bailey" */
+schema.virtual('lastName').get(function() {
+  return this.name.split(' ').slice(-1).join(' ');
+});
+
+module.exports = schema;
